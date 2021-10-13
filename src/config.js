50c49e3b6a39f6d49226fbef09db752b89124e7d
BlackDelga <evanbdelgado@gmail.com>
2021-10-12 23:11:12 -0400

    node install

--- src/config.js
+++ src/config.js
@@ -5,22 +5,22 @@ const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const description =
-  "This is the description of your NFT project, remember to replace this";
+	"This is the description of your NFT project, remember to replace this";
 const baseUri = "ipfs://NewUriToReplace";
 
 const layerConfigurations = [
-  {
-    growEditionSizeTo: 10,
-    layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
-    ],
-  },
+	{
+		growEditionSizeTo: 20,
+		layersOrder: [
+			{ name: "Background" },
+			{ name: "Eyeball" },
+			{ name: "Eye color" },
+			{ name: "Iris" },
+			{ name: "Shine" },
+			{ name: "Bottom lid" },
+			{ name: "Top lid" },
+		],
+	},
 ];
 
 const shuffleLayerConfigurations = false;
@@ -28,13 +28,13 @@ const shuffleLayerConfigurations = false;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+	width: 512,
+	height: 512,
 };
 
 const background = {
-  generate: true,
-  brightness: "80%",
+	generate: true,
+	brightness: "80%",
 };
 
 const extraMetadata = {};
@@ -44,22 +44,22 @@ const rarityDelimiter = "#";
 const uniqueDnaTorrance = 10000;
 
 const preview = {
-  thumbPerRow: 5,
-  thumbWidth: 50,
-  imageRatio: format.width / format.height,
-  imageName: "preview.png",
+	thumbPerRow: 5,
+	thumbWidth: 50,
+	imageRatio: format.width / format.height,
+	imageName: "preview.png",
 };
 
 module.exports = {
-  format,
-  baseUri,
-  description,
-  background,
-  uniqueDnaTorrance,
-  layerConfigurations,
-  rarityDelimiter,
-  preview,
-  shuffleLayerConfigurations,
-  debugLogs,
-  extraMetadata,
+	format,
+	baseUri,
+	description,
+	background,
+	uniqueDnaTorrance,
+	layerConfigurations,
+	rarityDelimiter,
+	preview,
+	shuffleLayerConfigurations,
+	debugLogs,
+	extraMetadata,
 };
