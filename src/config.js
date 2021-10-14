137de90a8998e68548a0bb556986f0b7334c6c2d
BlackDelga <evanbdelgado@gmail.com>
2021-10-14 18:41:18 -0400

    NFT's_Image

--- src/config.js
+++ src/config.js
@@ -5,61 +5,63 @@ const isLocal = typeof process.pkg === "undefined";
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const description =
-  "This is the description of your NFT project, remember to replace this";
-const baseUri = "ipfs://NewUriToReplace";
+	"After Saving earth numerous time Kid Goku goes on the coolest vacation";
+const baseUri = "ipfs://QmVeU3HvHbMfTjzNFDUA3iBaKbroEuRUQppnVAY91vhZ88";
 
 const layerConfigurations = [
-  {
-    growEditionSizeTo: 10,
-    layersOrder: [
-      { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
-    ],
-  },
+	{
+		growEditionSizeTo: 50,
+		layersOrder: [
+			{ name: "Background" },
+			{ name: "Goku_Base" },
+			{ name: "Goku_Outfit" },
+			{ name: "Goku_Chain" },
+			{ name: "Goku_Earring" },
+			{ name: "Goku_Eyes" },
+		],
+	},
 ];
 
-const shuffleLayerConfigurations = false;
+const shuffleLayerConfigurations = true;
 
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+	width: 1000,
+	height: 1000,
 };
 
 const background = {
-  generate: true,
-  brightness: "80%",
+	generate: true,
+	brightness: "80%",
 };
 
-const extraMetadata = {};
+const extraMetadata = {
+	design: "Evandro Delgado",
+	surname: "Blackdelga",
+};
 
 const rarityDelimiter = "#";
 
 const uniqueDnaTorrance = 10000;
 
 const preview = {
-  thumbPerRow: 5,
-  thumbWidth: 50,
-  imageRatio: format.width / format.height,
-  imageName: "preview.png",
+	thumbPerRow: 5,
+	thumbWidth: 50,
+	imageRatio: format.width / format.height,
+	imageName: "preview.png",
 };
 
 module.exports = {
-  format,
-  baseUri,
-  description,
-  background,
-  uniqueDnaTorrance,
-  layerConfigurations,
-  rarityDelimiter,
-  preview,
-  shuffleLayerConfigurations,
-  debugLogs,
-  extraMetadata,
+	format,
+	baseUri,
+	description,
+	background,
+	uniqueDnaTorrance,
+	layerConfigurations,
+	rarityDelimiter,
+	preview,
+	shuffleLayerConfigurations,
+	debugLogs,
+	extraMetadata,
 };
